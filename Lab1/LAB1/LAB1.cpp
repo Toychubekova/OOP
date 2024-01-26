d6214a546a423761421ca6d5e6de7cb1b06ee314
Aichurok <toychubekova2328.com>
2024-01-26 13:12:42 +0600

    add report

--- Lab1/LAB1/LAB1.cpp
+++ Lab1/LAB1/LAB1.cpp
@@ -49,11 +49,12 @@
 //}
 //
 //double power(char n, int p = 2) {
-//    return power(static_cast<double>(n), p);
-//}
+//    double result = 1.0;
 //
-//double power(short int n, int p = 2) {
-//    return power(static_cast<double>(n), p);
+//    for (int i = 0; i < p; ++i) {
+//        result *= static_cast<double>(n);
+//    }
+//    return result;
 //}
 //
 //double power(int n, int p = 2) {
@@ -71,8 +72,7 @@
 //int main() {
 //    cout << "double: " << power(2.5) << endl;
 //    cout << "char: " << power('A') << endl;
-//    cout << "short int: " << power((5)) << endl;
-//    cout << "int: " << power(5) << endl; 
+//    cout << "int: " << power(5) << endl;
 //    cout << "long int: " << power(10L) << endl;
 //    cout << "float: " << power(3.14f) << endl;
 //    return 0;
@@ -80,63 +80,63 @@
 
 // 3 задание
 
-//#include <iostream>
-//#include <vector>
-//#include <string>
-//using namespace std;
-//
-//struct Employee {
-//    string Name;
-//    int age;
-//    string specialty;
-//    double averageSalary;
-//};
-//
-//int main() {
-//    int N; 
-//    cout << "Enter the number of plants: ";
-//    cin >> N;
-//
-//    
-//    vector<vector<Employee>> factories(N);
-//
-//    for (int i = 0; i < N; ++i) {
-//        int M; 
-//        cout << "Enter the number of employees at the plant " << i + 1 << ": ";
-//        cin >> M;
-//
-//        for (int j = 0; j < M; ++j) {
-//            Employee employee;
-//            cout << "Enter the employee's name: ";
-//            cin >> employee.Name;
-//            cout << "Enter the employee's age: ";
-//            cin >> employee.age;
-//            cout << "Enter the employee's specialty: ";
-//            cin >> employee.specialty;
-//            cout << "Enter the employee's averge salary: ";
-//            cin >> employee.averageSalary;
-//
-//            factories[i].push_back(employee); 
-//        }
-//    }
-//
-//
-//    int numberOfSL = 0;
-//    int numberOfTOK = 0;
-//
-//    for (const auto& factory : factories) {
-//        for (const auto& employee : factory) {
-//            if (employee.specialty == "locksmith") {
-//                numberOfSL++;
-//            }
-//            else if (employee.specialty == "turner") {
-//                numberOfTOK++;
-//            }
-//        }
-//    }
-//    
-//    cout << "Number of locksmiths: " << numberOfSL << endl;
-//    cout << "Number of turners: " << numberOfTOK << endl;
-//
-//    return 0;
-//}
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+struct Employee {
+    string Name;
+    int age;
+    string specialty;
+    double averageSalary;
+};
+
+int main() {
+    int N; 
+    cout << "Enter the number of plants: ";
+    cin >> N;
+
+    
+    vector<vector<Employee>> factories(N);
+
+    for (int i = 0; i < N; ++i) {
+        int M; 
+        cout << "Enter the number of employees at the plant " << i + 1 << ": ";
+        cin >> M;
+
+        for (int j = 0; j < M; ++j) {
+            Employee employee;
+            cout << "Enter the employee's name: ";
+            cin >> employee.Name;
+            cout << "Enter the employee's age: ";
+            cin >> employee.age;
+            cout << "Enter the employee's specialty: ";
+            cin >> employee.specialty;
+            cout << "Enter the employee's averge salary: ";
+            cin >> employee.averageSalary;
+
+            factories[i].push_back(employee); 
+        }
+    }
+
+
+    int numberOfSL = 0;
+    int numberOfTOK = 0;
+
+    for (const auto& factory : factories) {
+        for (const auto& employee : factory) {
+            if (employee.specialty == "locksmith") {
+                numberOfSL++;
+            }
+            else if (employee.specialty == "turner") {
+                numberOfTOK++;
+            }
+        }
+    }
+    
+    cout << "Number of locksmiths: " << numberOfSL << endl;
+    cout << "Number of turners: " << numberOfTOK << endl;
+
+    return 0;
+}

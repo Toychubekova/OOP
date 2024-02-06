b030c94e9bad663fffce0c8b5ffee332776b9973
Aichurok <toychubekova2328.com>
2024-02-06 22:58:44 +0600

    add 3 laba

--- Lab2/LAB2/LAB2.cpp
+++ Lab2/LAB2/LAB2.cpp
@@ -1,224 +1,224 @@
 ﻿
 //1 задание
 
-#include <iostream>
-#include <iomanip>
-#include <vector>
-#include <algorithm>
-#include <clocale>
- #include <windows.h>
-using namespace std;
-
-class Aeroflot {
-public:
-    int flightNumber;
-    string departureCity;
-    string destinationCity;
-    string arrivalTime;
-    string departureTime;
-    int registrationSection;
-
-    Aeroflot(int number, const string& departure, const string& destination,
-        const string& arrival, const string& departureTime, int registrationSection)
-        : flightNumber(number), departureCity(departure), destinationCity(destination),
-        arrivalTime(arrival), departureTime(departureTime), registrationSection(registrationSection) {}
-
-    
-    void displayFlightInfo() const {
-        cout << setw(6) << flightNumber << setw(15) << departureCity << setw(15) << destinationCity
-            << setw(15) << arrivalTime << setw(15) << departureTime << setw(15) << registrationSection << endl;
-    }
-};
-
-int main() {
-    setlocale(LC_ALL, "Russian");
-    vector<Aeroflot> flights;
-    SetConsoleOutputCP(1251);
-    SetConsoleCP(1251);
-    int numberOfFlights;
-
-    cout << "Введите количество рейсов: ";
-    cin >> numberOfFlights;
-
-    for (int i = 0; i < numberOfFlights; ++i) {
-        int number, regSection;
-        string departure, destination, arrival, departureTime;
-
-        cout << "Введите номер рейса: ";
-        cin >> number;
-
-        cout << "Введите пункт отправления: ";
-        cin >> departure;
-
-        cout << "Введите пункт назначения: ";
-        cin >> destination;
-
-        cout << "Введите время прибытия: ";
-        cin >> arrival;
-
-        cout << "Введите время отправления: ";
-        cin >> departureTime;
-
-        cout << "Введите номер секции для регистрации: ";
-        cin >> regSection;
-
-        flights.emplace_back(number, departure, destination, arrival, departureTime, regSection);
-    }
-
-    
-    sort(flights.begin(), flights.end(), [](const Aeroflot& a, const Aeroflot& b) {
-        return a.destinationCity < b.destinationCity;
-        });
-
-  
-    cout << "\nСписок рейсов:\n";
-    cout << setw(6) << "Номер" << setw(15) << "Отправление" << setw(15) << "Назначение"
-        << setw(15) << "Прибытие" << setw(15) << "Отправление" << setw(15) << "Регистрация\n";
-
-    if (!flights.empty()) {
-        for (const auto& flight : flights) {
-            flight.displayFlightInfo();
-        }
-    }
-    else {
-        cout << "Нет доступных рейсов.\n";
-    }
-
-    return 0;
-}
-
-// /*2 задание*/
 //#include <iostream>
+//#include <iomanip>
 //#include <vector>
 //#include <algorithm>
-//#include <windows.h>
+//#include <clocale>
+// #include <windows.h>
 //using namespace std;
 //
-//class Student {
+//class Aeroflot {
 //public:
-//    string lastName;
-//    string dateOfBirth;
-//    string phoneNumber;
+//    int flightNumber;
+//    string departureCity;
+//    string destinationCity;
+//    string arrivalTime;
+//    string departureTime;
+//    int registrationSection;
 //
-//    Student(const string& last, const string& dob, const string& phone)
-//        : lastName(last), dateOfBirth(dob), phoneNumber(phone) {}
+//    Aeroflot(int number, const string& departure, const string& destination,
+//        const string& arrival, const string& departureTime, int registrationSection)
+//        : flightNumber(number), departureCity(departure), destinationCity(destination),
+//        arrivalTime(arrival), departureTime(departureTime), registrationSection(registrationSection) {}
 //
-//    void displayInfo() const {
-//        cout << "Фамилия: " << lastName << "\n";
-//        cout << "Дата рождения: " << dateOfBirth << "\n";
-//        cout << "Телефон: " << phoneNumber << "\n";
-//    }
-//};
-//
-//class StudentGroup {
-//private:
-//    vector<Student> students;
-//
-//public:
-//    void addStudent(const string& lastName, const string& dateOfBirth, const string& phoneNumber) {
-//        students.emplace_back(lastName, dateOfBirth, phoneNumber);
-//    }
-//
-//    void removeStudent(const string& lastName) {
-//        students.erase(remove_if(students.begin(), students.end(),
-//            [&](const Student& student) { return student.lastName == lastName; }),
-//            students.end());
-//    }
-//
-//    Student* findStudentByLastName(const string& lastName) {
-//        auto it = find_if(students.begin(), students.end(),
-//            [&](const Student& student) { return student.lastName == lastName; });
-//
-//        return (it != students.end()) ? &(*it) : nullptr;
-//    }
-//
-//    void sortStudentsByLastName() {
-//        sort(students.begin(), students.end(),
-//            [](const Student& a, const Student& b) { return a.lastName < b.lastName; });
-//    }
-//
-//    void displayAllStudents() const {
-//        if (students.empty()) {
-//            cout << "Группа пуста." << endl;
-//        }
-//        else {
-//            cout << "Список студентов:\n";
-//            for (const auto& student : students) {
-//                student.displayInfo();
-//                cout << "\n";
-//            }
-//        }
+//    
+//    void displayFlightInfo() const {
+//        cout << setw(6) << flightNumber << setw(15) << departureCity << setw(15) << destinationCity
+//            << setw(15) << arrivalTime << setw(15) << departureTime << setw(15) << registrationSection << endl;
 //    }
 //};
 //
 //int main() {
 //    setlocale(LC_ALL, "Russian");
-//    StudentGroup group;
+//    vector<Aeroflot> flights;
 //    SetConsoleOutputCP(1251);
 //    SetConsoleCP(1251);
-//    int choice;
-//    do {
-//        cout << "\nМеню:\n";
-//        cout << "1. Добавить студента\n";
-//        cout << "2. Удалить студента\n";
-//        cout << "3. Найти студента по фамилии\n";
-//        cout << "4. Сортировать студентов по фамилии\n";
-//        cout << "5. Показать всех студентов\n";
-//        cout << "0. Выйти\n";
-//        cout << "Выберите действие: ";
-//        cin >> choice;
+//    int numberOfFlights;
 //
-//        switch (choice) {
-//        case 1: {
-//            string lastName, dateOfBirth, phoneNumber;
-//            cout << "Введите фамилию: ";
-//            cin >> lastName;
-//            cout << "Введите дату рождения: ";
-//            cin >> dateOfBirth;
-//            cout << "Введите номер телефона: ";
-//            cin >> phoneNumber;
-//            group.addStudent(lastName, dateOfBirth, phoneNumber);
-//            break;
-//        }
-//        case 2: {
-//            string lastName;
-//            cout << "Введите фамилию студента, которого нужно удалить: ";
-//            cin >> lastName;
-//            group.removeStudent(lastName);
-//            break;
-//        }
-//        case 3: {
-//            string lastName;
-//            cout << "Введите фамилию студента, которого нужно найти: ";
-//            cin >> lastName;
-//            Student* foundStudent = group.findStudentByLastName(lastName);
-//            if (foundStudent) {
-//                cout << "Студент найден:\n";
-//                foundStudent->displayInfo();
-//            }
-//            else {
-//                cout << "Студент не найден.\n";
-//            }
-//            break;
-//        }
-//        case 4:
-//            group.sortStudentsByLastName();
-//            cout << "Студенты отсортированы по фамилии.\n";
-//            break;
-//        case 5:
-//            group.displayAllStudents();
-//            break;
-//        case 0:
-//            cout << "Программа завершена.\n";
-//            break;
-//        default:
-//            cout << "Неверный ввод. Повторите попытку.\n";
-//        }
+//    cout << "Введите количество рейсов: ";
+//    cin >> numberOfFlights;
 //
-//    } while (choice != 0);
+//    for (int i = 0; i < numberOfFlights; ++i) {
+//        int number, regSection;
+//        string departure, destination, arrival, departureTime;
+//
+//        cout << "Введите номер рейса: ";
+//        cin >> number;
+//
+//        cout << "Введите пункт отправления: ";
+//        cin >> departure;
+//
+//        cout << "Введите пункт назначения: ";
+//        cin >> destination;
+//
+//        cout << "Введите время прибытия: ";
+//        cin >> arrival;
+//
+//        cout << "Введите время отправления: ";
+//        cin >> departureTime;
+//
+//        cout << "Введите номер секции для регистрации: ";
+//        cin >> regSection;
+//
+//        flights.emplace_back(number, departure, destination, arrival, departureTime, regSection);
+//    }
+//
+//    
+//    sort(flights.begin(), flights.end(), [](const Aeroflot& a, const Aeroflot& b) {
+//        return a.destinationCity < b.destinationCity;
+//        });
+//
+//  
+//    cout << "\nСписок рейсов:\n";
+//    cout << setw(6) << "Номер" << setw(15) << "Отправление" << setw(15) << "Назначение"
+//        << setw(15) << "Прибытие" << setw(15) << "Отправление" << setw(15) << "Регистрация\n";
+//
+//    if (!flights.empty()) {
+//        for (const auto& flight : flights) {
+//            flight.displayFlightInfo();
+//        }
+//    }
+//    else {
+//        cout << "Нет доступных рейсов.\n";
+//    }
 //
 //    return 0;
 //}
+
+// /*2 задание*/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <windows.h>
+using namespace std;
+
+class Student {
+public:
+    string lastName;
+    string dateOfBirth;
+    string phoneNumber;
+
+    Student(const string& last, const string& dob, const string& phone)
+        : lastName(last), dateOfBirth(dob), phoneNumber(phone) {}
+
+    void displayInfo() const {
+        cout << "Фамилия: " << lastName << "\n";
+        cout << "Дата рождения: " << dateOfBirth << "\n";
+        cout << "Телефон: " << phoneNumber << "\n";
+    }
+};
+
+class StudentGroup {
+private:
+    vector<Student> students;
+
+public:
+    void addStudent(const string& lastName, const string& dateOfBirth, const string& phoneNumber) {
+        students.emplace_back(lastName, dateOfBirth, phoneNumber);
+    }
+
+    void removeStudent(const string& lastName) {
+        students.erase(remove_if(students.begin(), students.end(),
+            [&](const Student& student) { return student.lastName == lastName; }),
+            students.end());
+    }
+
+    Student* findStudentByLastName(const string& lastName) {
+        auto it = find_if(students.begin(), students.end(),
+            [&](const Student& student) { return student.lastName == lastName; });
+
+        return (it != students.end()) ? &(*it) : nullptr;
+    }
+
+    void sortStudentsByLastName() {
+        sort(students.begin(), students.end(),
+            [](const Student& a, const Student& b) { return a.lastName < b.lastName; });
+    }
+
+    void displayAllStudents() const {
+        if (students.empty()) {
+            cout << "Группа пуста." << endl;
+        }
+        else {
+            cout << "Список студентов:\n";
+            for (const auto& student : students) {
+                student.displayInfo();
+                cout << "\n";
+            }
+        }
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    StudentGroup group;
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    int choice;
+    do {
+        cout << "\nМеню:\n";
+        cout << "1. Добавить студента\n";
+        cout << "2. Удалить студента\n";
+        cout << "3. Найти студента по фамилии\n";
+        cout << "4. Сортировать студентов по фамилии\n";
+        cout << "5. Показать всех студентов\n";
+        cout << "0. Выйти\n";
+        cout << "Выберите действие: ";
+        cin >> choice;
+
+        switch (choice) {
+        case 1: {
+            string lastName, dateOfBirth, phoneNumber;
+            cout << "Введите фамилию: ";
+            cin >> lastName;
+            cout << "Введите дату рождения: ";
+            cin >> dateOfBirth;
+            cout << "Введите номер телефона: ";
+            cin >> phoneNumber;
+            group.addStudent(lastName, dateOfBirth, phoneNumber);
+            break;
+        }
+        case 2: {
+            string lastName;
+            cout << "Введите фамилию студента, которого нужно удалить: ";
+            cin >> lastName;
+            group.removeStudent(lastName);
+            break;
+        }
+        case 3: {
+            string lastName;
+            cout << "Введите фамилию студента, которого нужно найти: ";
+            cin >> lastName;
+            Student* foundStudent = group.findStudentByLastName(lastName);
+            if (foundStudent) {
+                cout << "Студент найден:\n";
+                foundStudent->displayInfo();
+            }
+            else {
+                cout << "Студент не найден.\n";
+            }
+            break;
+        }
+        case 4:
+            group.sortStudentsByLastName();
+            cout << "Студенты отсортированы по фамилии.\n";
+            break;
+        case 5:
+            group.displayAllStudents();
+            break;
+        case 0:
+            cout << "Программа завершена.\n";
+            break;
+        default:
+            cout << "Неверный ввод. Повторите попытку.\n";
+        }
+
+    } while (choice != 0);
+
+    return 0;
+}
 
 //3 задание
 
---
+++ Lab3/Lab3/Lab3.cpp
@@ -0,0 +1,219 @@
+﻿
+//1 задание
+//#include <iostream>
+//#include <ctime>
+//#include <cstdlib>
+//#include <iomanip>
+//
+//using namespace std;
+//
+//class Timer {
+//private:
+//    int seconds;
+//
+//public:
+//    
+//    Timer(int initialSeconds) : seconds(initialSeconds) {}
+//
+//    Timer(const string& timeString) {
+//        parseTimeString(timeString);
+//    }
+//
+//    Timer(int minutes, int seconds) : seconds(minutes * 60 + seconds) {}
+//
+//    void run() {
+//        while (seconds > 0) {
+//            displayTime();
+//            sleep(1);  // Задержка на 1 секунду
+//            --seconds;
+//        }
+//        cout << "\aTime's up! Ding ding ding!" << endl;  // Звонок
+//    }
+//
+//private:
+//   
+//    void displayTime() const {
+//        int minutes = seconds / 60;
+//        int remainingSeconds = seconds % 60;
+//        cout << setw(2) << setfill('0') << minutes << ":"
+//            << setw(2) << setfill('0') << remainingSeconds << "\r" << flush;
+//    }
+//
+//    
+//    void parseTimeString(const string& timeString) {
+//        size_t colonPos = timeString.find(':');
+//        if (colonPos != string::npos) {
+//            char* end;
+//            long minutes = strtol(timeString.c_str(), &end, 10);
+//            if (*end == ':' && end != timeString.c_str()) {
+//                // Преобразование минут успешно, продолжаем с секундами
+//                long seconds = strtol(end + 1, &end, 10);
+//                if (*end == '\0') {
+//                    // Преобразование секунд успешно, устанавливаем значение таймера
+//                    this->seconds = minutes * 60 + seconds;
+//                    return;
+//                }
+//            }
+//        }
+//
+//        
+//        cerr << "Invalid time string format. Use \"mm:ss\"." << endl;
+//        exit(EXIT_FAILURE);
+//    }
+//
+//    
+//    void sleep(int seconds) const {
+//        clock_t delay = seconds * CLOCKS_PER_SEC;
+//        clock_t start = clock();
+//        while ((clock() - start) < delay) {}
+//    }
+//};
+//
+//int main() {
+//    // Примеры использования конструкторов
+//    Timer timer1(180);  // Таймер на 3 минуты
+//    Timer timer2("05:30");  // Таймер на 5 минут 30 секунд
+//    Timer timer3(2, 30);  // Таймер на 2 минуты 30 секунд
+//
+//    // Запуск таймеров
+//    cout << "Timer 1: ";
+//    timer1.run();
+//
+//    cout << "Timer 2: ";
+//    timer2.run();
+//
+//    cout << "Timer 3: ";
+//    timer3.run();
+//
+//    return 0;
+//}
+
+// 2 задание
+//#include <iostream>
+//#include <string>
+//
+//using namespace std;
+//
+//class Nomenclature {
+//private:
+//    string productName;
+//    double wholesalePrice;
+//    double retailMarkup;
+//    int quantityInStock;
+//
+//public:
+//    
+//    Nomenclature() : productName(""), wholesalePrice(0.0), retailMarkup(0.0), quantityInStock(0) {}
+//
+//    
+//    Nomenclature(const string& name, double wholesale, double markup, int quantity)
+//        : productName(name), wholesalePrice(wholesale), retailMarkup(markup), quantityInStock(quantity) {}
+//
+//   
+//    ~Nomenclature() {
+//        
+//    }
+//
+//    double calculateNetIncome(int quantitySold) const {
+//        double retailPrice = calculateRetailPrice();
+//        return quantitySold * (retailPrice - wholesalePrice);
+//    }
+//
+//    
+//    void displayProductInfo() const {
+//        cout << "Название: " << productName << endl;
+//        cout << "Оптовая цена: $" << wholesalePrice << endl;
+//        cout << "Розничная наценка: " << retailMarkup << "%" << endl;
+//        cout << "Количество на складе: " << quantityInStock << endl;
+//        cout << "Розничная цена: $" << calculateRetailPrice() << endl;
+//    }
+//
+//private:
+//    
+//    double calculateRetailPrice() const {
+//        return wholesalePrice * (1 + retailMarkup / 100);
+//    }
+//};
+//
+//int main() {
+//    setlocale(LC_ALL, "Russian");
+//    
+//    Nomenclature product1("Фотоаппарат", 800.0, 20.0, 50);
+//    product1.displayProductInfo();
+//
+//    cout << "Чистый доход от продажи 10 единиц: $" << product1.calculateNetIncome(10) << endl;
+//
+//    return 0;
+//}
+
+// 3 задание
+//#include <iostream>
+//#include <string>
+//#include <ctime>
+//
+//using namespace std;
+//
+//class Soft {
+//private:
+//    string programName;
+//    string developer;
+//    double occupiedSpace;
+//    time_t licenseExpiration;
+//
+//public:
+//    
+//    Soft(const string& name, const string& dev, double space, time_t expiration)
+//        : programName(name), developer(dev), occupiedSpace(space), licenseExpiration(expiration) {}
+//
+//    
+//    ~Soft() {
+//        cout << "Программа " << programName << " удалена." << endl;
+//    }
+//
+//    
+//    int daysUntilLicenseExpiration() const {
+//        time_t currentTime;
+//        time(&currentTime);
+//
+//        int secondsRemaining;
+//        if (difftime(licenseExpiration, currentTime) > 0) {
+//            secondsRemaining = static_cast<int>(difftime(licenseExpiration, currentTime));
+//        }
+//        else {
+//            secondsRemaining = 0;
+//        }
+//
+//        int daysRemaining = secondsRemaining / (60 * 60 * 24);
+//
+//        return daysRemaining;
+//    }
+//
+//    void displaySoftwareInfo() const {
+//        cout << "Название программы: " << programName << endl;
+//        cout << "Разработчик: " << developer << endl;
+//        cout << "Занимаемый объем: " << occupiedSpace << " MB" << endl;
+//
+//        char buffer[26];
+//        ctime_s(buffer, sizeof(buffer), &licenseExpiration);
+//        cout << "Дата завершения лицензии: " << buffer;
+//    }
+//};
+//
+//int main() {
+//    setlocale(LC_ALL, "Russian");
+//
+//    time_t expirationTime;
+//    time(&expirationTime);
+//    expirationTime += 30 * 24 * 60 * 60;  // Лицензия на 30 дней от текущего момента
+//
+//    Soft software1("PyCharm", "Тойчубекова Айчурок", 160.5, expirationTime);
+//
+//
+//    cout << "Информация о программном обеспечении:" << endl;
+//    software1.displaySoftwareInfo();
+//
+//    int daysRemaining = software1.daysUntilLicenseExpiration();
+//    cout << "Дней до завершения лицензии: " << daysRemaining << endl;
+//
+//    return 0;
+//}

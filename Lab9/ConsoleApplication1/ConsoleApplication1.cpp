200cb32f42b14bdfd676be1e212f59078af58de0
Aichurok <toychubekova2328.com>
2024-04-26 11:35:54 +0600

    add

--- Lab9/ConsoleApplication1/ConsoleApplication1.cpp
+++ Lab9/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,67 +1,73 @@
-﻿#include <iostream>
-#include <string>
+﻿//#include <iostream>
+//#include <string>
+//
+//using namespace std;
+//
+//const int LIMIT = 5;
+//
+//class SafeArray {
+//private:
+//    int arr[LIMIT];
+//public:
+//    class Range : public exception {
+//    private:
+//        string msg;
+//    public:
+//        Range(const string& message) : msg(message) {}/* используется для обработки ошибок при выходе за пределы массива*/
+//        const string& getMessage() const {
+//            return msg;
+//        }
+//    };
+//
+//    void putel(int n, int value) {
+//        if (n < 0 || n >= LIMIT) {
+//            throw Range("Ошибочный индекс #" + to_string(n));
+//        }
+//
+//        arr[n] = value;
+//    }
+//
+//    int getel(int n) const {
+//        if (n < 0 || n >= LIMIT) {
+//            throw Range("Ошибочный индекс #" + to_string(n));
+//        }
+//
+//        return arr[n];
+//    }
+//};
+//
+//int main() {
+//    setlocale(LC_ALL, "ru");
+//
+//    SafeArray arr;
+//
+//    try {
+//        for (int i = 0; i < LIMIT; i++) {
+//            arr.putel(i, i * 10);
+//        }
+//
+//        for (int i = 0; i < LIMIT + 1; i++) {
+//            cout << "Элемент #" << i << " = " << arr.getel(i) << endl;
+//        }
+//    }
+//    catch (const exception& e) {
+//        cout << "Ошибка: " << e.what() << endl;
+//    }
+//
+//
+//
+//    return 0;
+//}
 
-using namespace std;
-
-const int LIMIT = 5;
-
-class SafeArray {
-private:
-    int arr[LIMIT];
-public:
-    class Range : public exception {
-    private:
-        string msg;
-    public:
-        Range(const string& message) : msg(message) {}/* используется для обработки ошибок при выходе за пределы массива*/
-        const string& getMessage() const {
-            return msg;
-        }
-    };
-
-    void putel(int n, int value) {
-        if (n < 0 || n >= LIMIT) {
-            // cout << "\nОшибочный индекс #";
-             //exit(1);
-            throw Range("Ошибочный индекс #" + to_string(n));
-        }
-
-        arr[n] = value;
-    }
-
-    int getel(int n) const {
-        if (n < 0 || n >= LIMIT) {
-            //cout << "\nОшибочный индекс #";
-            //exit(1);
-            throw Range("Ошибочный индекс #" + to_string(n));
-        }
-
-        return arr[n];
-    }
-};
-
-int main() {
-    setlocale(LC_ALL, "ru");
-
-    SafeArray arr;
-
-    try {
-        for (int i = 0; i < LIMIT; i++) {
-            arr.putel(i, i * 10);
-        }
-
-        for (int i = 0; i < LIMIT + 1; i++) {
-            cout << "Элемент #" << i << " = " << arr.getel(i) << endl;
-        }
-    }
-    catch (const exception& e) {
-        //cout << "Ошибка: " << e.getMessage() << endl;
-        cout << "Ошибка: " << e.what() << endl;
-    }
-
-
-    return 0;
-}
+//
+//char a;
+//try {
+//    cin >> a;
+//}
+//catch (const std::exception& e) {
+//    cout << "Ошибка: " << e.what() << endl;
+//
+//}
 
 
 #include <iostream>
@@ -152,3 +158,82 @@ int main() {
 //
 //    return 0;
 //}
+
+//#include <iostream>
+//#include <cstdlib>
+//#include <ctime>
+//
+//class Gun {
+//private:
+//    static double p; // Вероятность попадания
+//public:
+//    static int shoot(int n) {
+//        srand(static_cast<unsigned int>(time(0)));
+//        for (int i = 0; i < n; ++i) {
+//            if ((double)rand() / RAND_MAX < p) {
+//                return 1; // Самолет сбит
+//            }
+//        }
+//        return 0; // Самолет не сбит
+//    }
+//
+//    static double estimateProbability(int n) {
+//        return 1 - pow((1 - p), n);
+//    }
+//
+//    static void setProbability(double probability) {
+//        p = probability;
+//    }
+//};
+//
+//double Gun::p = 0.0; // Инициализация статического поля
+//
+//int main() {
+//    Gun::setProbability(0.3); // Устанавливаем вероятность попадания
+//
+//    int n;
+//    std::cout << "Введите количество выстрелов: ";
+//    std::cin >> n;
+//
+//    int result = Gun::shoot(n);
+//    std::cout << "Результат стрельбы: " << (result == 1 ? "Цель уничтожена" : "Цель не поражена") << std::endl;
+//
+//    double estimatedProbability = Gun::estimateProbability(n);
+//    std::cout << "Оценочное значение вероятности поражения цели: " << estimatedProbability << std::endl;
+//
+//    return 0;
+//}
+#include <iostream>
+#include <stdexcept>
+
+class Parent {
+private:
+    static int staticField;
+public:
+    static int& getStaticField() {
+        return staticField;
+    }
+};
+
+int Parent::staticField = 10;
+
+class Child : private Parent {
+public:
+    void displayStaticField() {
+        try {
+            int fieldValue = Parent::getStaticField(); // Доступ к приватному полю через метод родительского класса
+            std::cout << "Value of staticField in Parent: " << fieldValue << std::endl;
+        }
+        catch (const std::exception& e) {
+            std::cerr << "Exception caught: " << e.what() << std::endl;
+        }
+    }
+};
+
+int main() {
+    Child childObj;
+    childObj.displayStaticField();
+
+    return 0;
+}
+
---
+++ individ/individ/individ.cpp
@@ -0,0 +1,416 @@
+﻿
+//
+//#include <iostream>
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//class Person {
+//private:
+//    string name;
+//    string registrationNumber;
+//    string registrationDate;
+//    string personalInfo;
+//    string partnerRequirements;
+//    string gender;
+//    bool hasMet; // Флаг, указывающий, участвовал ли кандидат во встрече
+//
+//public:
+//    Person(const string& n, const string& regNum, const string& regDate,
+//        const string& pInfo, const string& pReq, const string& g)
+//        : name(n), registrationNumber(regNum), registrationDate(regDate),
+//        personalInfo(pInfo), partnerRequirements(pReq), gender(g), hasMet(false) {}
+//
+//    void display() const {
+//        cout << "Name: " << name << endl;
+//        cout << "Registration Number: " << registrationNumber << endl;
+//        cout << "Registration Date: " << registrationDate << endl;
+//        cout << "Personal Info: " << personalInfo << endl;
+//        cout << "Partner Requirements: " << partnerRequirements << endl;
+//        cout << "Gender: " << gender << endl;
+//    }
+//
+//    string getPartnerRequirements() const {
+//        return partnerRequirements;
+//    }
+//
+//    string getGender() const {
+//        return gender;
+//    }
+//
+//    string getPersonalInfo() const {
+//        return personalInfo;
+//    }
+//
+//    bool hasMetBefore() const {
+//        return hasMet;
+//    }
+//
+//    void setMet(bool value) {
+//        hasMet = value;
+//    }
+//};
+//
+//class MatchmakingAgency {
+//private:
+//    vector<Person> potentialMatches;
+//    vector<Person> archive; // Для хранения архивированных кандидатов
+//
+//public:
+//    void addPotentialMatch(const Person& person) {
+//        potentialMatches.push_back(person);
+//    }
+//
+//    void displayPotentialMatches() const {
+//        for (const auto& person : potentialMatches) {
+//            person.display();
+//            cout << endl;
+//        }
+//    }
+//
+//    void deletePotentialMatch(int index) {
+//        if (index >= 0 && index < potentialMatches.size()) {
+//            potentialMatches.erase(potentialMatches.begin() + index);
+//            cout << "Candidate successfully removed." << endl;
+//        }
+//        else {
+//            cout << "Invalid index." << endl;
+//        }
+//    }
+//
+//    void prepareMeetings() {
+//        bool meetingFound = false;
+//        for (size_t i = 0; i < potentialMatches.size(); ++i) {
+//            for (size_t j = i + 1; j < potentialMatches.size(); ++j) {
+//                if (potentialMatches[i].getPartnerRequirements() == potentialMatches[j].getPersonalInfo() &&
+//                    potentialMatches[i].getGender() != potentialMatches[j].getGender()) {
+//                    cout << "Meeting prepared between:" << endl;
+//                    potentialMatches[i].display();
+//                    potentialMatches[j].display();
+//                    cout << endl;
+//                    meetingFound = true;
+//
+//                    // Помечаем кандидатов как участвующих во встрече
+//                    potentialMatches[i].setMet(true);
+//                    potentialMatches[j].setMet(true);
+//                }
+//            }
+//        }
+//        if (!meetingFound) {
+//            cout << "No compatible matches found for a meeting." << endl;
+//        }
+//    }
+//
+//    void archiveMatch() {
+//        for (auto it = potentialMatches.begin(); it != potentialMatches.end();) {
+//            if (it->hasMetBefore()) {
+//                archive.push_back(*it);
+//                it = potentialMatches.erase(it);
+//            }
+//            else {
+//                ++it;
+//            }
+//        }
+//        cout << "Candidates who participated in meetings are archived." << endl;
+//    }
+//
+//    void displayArchive() const {
+//        for (const auto& person : archive) {
+//            person.display();
+//            cout << endl;
+//        }
+//    }
+//};
+//
+//int main() {
+//    MatchmakingAgency agency;
+//
+//    int choice;
+//    do {
+//        cout << "Menu:" << endl;
+//        cout << "1. Add candidate" << endl;
+//        cout << "2. Show all candidates" << endl;
+//        cout << "3. Delete candidate" << endl;
+//        cout << "4. Prepare meetings" << endl;
+//        cout << "5. Archive candidates who participated in meetings" << endl;
+//        cout << "6. Display archived candidates" << endl;
+//        cout << "7. Exit" << endl;
+//        cout << "Choose an action: ";
+//        cin >> choice;
+//
+//        switch (choice) {
+//        case 1: {
+//            string name, regNum, regDate, pInfo, pReq;
+//            int genderChoice;
+//            cout << "Enter name: ";
+//            cin.ignore();
+//            getline(cin, name);
+//            cout << "Enter registration number: ";
+//            cin >> regNum;
+//            cout << "Enter registration date: ";
+//            cin.ignore();
+//            getline(cin, regDate);
+//            cout << "Enter personal info: ";
+//            getline(cin, pInfo);
+//            cout << "Choose partner requirements:" << endl;
+//            cout << "1. Caring" << endl;
+//            cout << "2. Cute" << endl;
+//            cout << "3. Tall" << endl;
+//            cout << "4. With a sense of humor" << endl;
+//            cout << "Enter number for partner requirements: ";
+//            int reqChoice;
+//            cin >> reqChoice;
+//            switch (reqChoice) {
+//            case 1:
+//                pReq = "Caring";
+//                break;
+//            case 2:
+//                pReq = "Cute";
+//                break;
+//            case 3:
+//                pReq = "Tall";
+//                break;
+//            case 4:
+//                pReq = "With a sense of humor";
+//                break;
+//            default:
+//                cout << "Invalid choice." << endl;
+//                continue;
+//            }
+//            cout << "Choose gender:" << endl;
+//            cout << "1. Male" << endl;
+//            cout << "2. Female" << endl;
+//            cout << "Enter number for gender: ";
+//            cin >> genderChoice;
+//            string gender;
+//            switch (genderChoice) {
+//            case 1:
+//                gender = "Male";
+//                break;
+//            case 2:
+//                gender = "Female";
+//                break;
+//            default:
+//                cout << "Invalid choice." << endl;
+//                continue;
+//            }
+//            Person newCandidate(name, regNum, regDate, pInfo, pReq, gender);
+//            agency.addPotentialMatch(newCandidate);
+//            break;
+//        }
+//        case 2:
+//            agency.displayPotentialMatches();
+//            break;
+//        case 3: {
+//            int index;
+//            cout << "Enter index of candidate to delete: ";
+//            cin >> index;
+//            agency.deletePotentialMatch(index);
+//            break;
+//        }
+//        case 4:
+//            agency.prepareMeetings();
+//            break;
+//        case 5:
+//            agency.archiveMatch();
+//            break;
+//        case 6:
+//            cout << "Archived candidates:" << endl;
+//            agency.displayArchive();
+//            break;
+//        case 7:
+//            cout << "Exiting the program." << endl;
+//            break;
+//        default:
+//            cout << "Invalid choice" << endl;
+//        }
+//    } while (choice != 7);
+//
+//    return 0;
+//}
+//
+//
+
+
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+
+using namespace std;
+
+class Prisoner {
+private:
+    string name;
+    string article;
+    int sentence;
+    string incarcerationDate;
+    string hierarchy;
+    string cell;
+    string relatives;
+    string personalityTraits;
+
+public:
+    Prisoner(string name, string article, int sentence, string incarcerationDate, string hierarchy, string cell, string relatives, string personalityTraits)
+        : name(name), article(article), sentence(sentence), incarcerationDate(incarcerationDate), hierarchy(hierarchy), cell(cell), relatives(relatives), personalityTraits(personalityTraits) {}
+
+    void displayInfo() const {
+        cout << "Имя: " << name << endl;
+        cout << "Преступление: " << article << endl;
+        cout << "Срок: " << sentence << " года(лет)" << endl;
+        cout << "Дата заключения: " << incarcerationDate << endl;
+        cout << "Позиция в иерархии: " << hierarchy << endl;
+        cout << "Камера: " << cell << endl;
+        cout << "Родственники: " << relatives << endl;
+        cout << "Характерные черты: " << personalityTraits << endl;
+        cout << "-----------------------------------" << endl;
+    }
+
+    string getName() const { return name; }
+    string getHierarchy() const { return hierarchy; }
+    string getArticle() const { return article; }
+};
+
+class Prison {
+private:
+    vector
+        <Prisoner> prisoners;
+
+public:
+    void addPrisoner(const Prisoner& prisoner) {
+        prisoners.push_back(prisoner);
+    }
+
+    void displayAllPrisoners() const {
+        for (const auto& prisoner : prisoners) {
+            prisoner.displayInfo();
+        }
+    }
+
+    void searchByAttribute(const string& attribute) const {
+        for (const auto& prisoner : prisoners) {
+            if (prisoner.getHierarchy() == attribute) {
+                prisoner.displayInfo();
+            }
+        }
+    }
+
+    void generateCrimeStats() const {
+        map<string, int> crimeCounts;
+        for (const auto& prisoner : prisoners) {
+            crimeCounts[prisoner.getArticle()]++;
+        }
+
+        cout << "\nСтатистика преступлений:\n";
+        for (const auto& pair : crimeCounts) {
+            cout << pair.first << ": " << pair.second << " заключенных(ых)\n";
+        }
+    }
+
+    void searchByArbitraryAttribute() const {
+        string attributeName, attributeValue;
+        cin.ignore(); // очистка буфера ввода
+        cout << "\nВведите название атрибута для поиска: ";
+        getline(cin, attributeName);
+        cout << "Введите значение атрибута: ";
+        getline(cin, attributeValue);
+
+        cout << "\nРезультаты поиска:\n";
+        for (const auto& prisoner : prisoners) {
+            if (attributeName == "Имя" && prisoner.getName() == attributeValue) {
+                prisoner.displayInfo();
+            }
+            else if (attributeName == "Позиция в иерархии" && prisoner.getHierarchy() == attributeValue) {
+                prisoner.displayInfo();
+            }
+            else if (attributeName == "Преступление" && prisoner.getArticle() == attributeValue) {
+                prisoner.displayInfo();
+            }
+        }
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    Prison prison;
+
+    while (true) {
+        cout << "\nМеню:\n";
+        cout << "1. Добавить заключенного\n";
+        cout << "2. Показать всех заключенных\n";
+        cout << "3. Поиск заключенных по позиции в иерархии\n";
+        cout << "4. Генерировать статистику по преступлениям\n";
+        cout << "5. Поиск заключенных по произвольному атрибуту\n";
+        cout << "6. Выход\n";
+        cout << "Выберите опцию: ";
+
+        int choice;
+        cin >> choice;
+        cin.ignore();
+
+        switch (choice) {
+        case 1: {
+            string name, article, incarcerationDate, hierarchy, cell, relatives, personalityTraits;
+            int sentence;
+            cout << "\nВведите информацию о новом заключенном:\n";
+            cout << "Имя: ";
+            getline(cin, name);
+            cout << "Преступление: ";
+            getline(cin, article);
+            cout << "Срок (в годах): ";
+            cin >> sentence;
+            cin.ignore();
+            cout << "Дата заключения (ДД.ММ.ГГГГ): ";
+            getline(cin, incarcerationDate);
+            cout << "Позиция в иерархии: ";
+            getline(cin, hierarchy);
+            cout << "Камера: ";
+            getline(cin, cell);
+            cout << "Родственники: ";
+            getline(cin, relatives);
+            cout << "Характерные черты: ";
+            getline(cin, personalityTraits);
+
+            Prisoner newPrisoner(name, article, sentence, incarcerationDate, hierarchy, cell, relatives, personalityTraits);
+            prison.addPrisoner(newPrisoner);
+            break;
+        }
+        case 2: {
+            cout << "\nИнформация о всех заключенных:\n";
+            prison.displayAllPrisoners();
+            break;
+        }
+        case 3: {
+            string searchAttribute;
+            cout << "\nВведите позицию в иерархии для поиска заключенных: ";
+            getline(cin, searchAttribute);
+            cout << "\nРезультаты поиска:\n";
+            prison.searchByAttribute(searchAttribute);
+            break;
+        }
+        case 4: {
+            prison.generateCrimeStats();
+            break;
+        }
+        case 5: {
+            prison.searchByArbitraryAttribute();
+            break;
+        }
+        case 6: {
+            cout << "\nПрограмма завершена.\n";
+            return 0;
+        }
+        default:
+            cout << "Неверный выбор. Пожалуйста, выберите опцию из меню.\n";
+        }
+    }
+
+    return 0;
+}
+
+
+
+
+
